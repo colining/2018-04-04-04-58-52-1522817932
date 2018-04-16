ba387621a43af7e717fbe0384b0ed0c6e68a0b03
colining <18810782954@163.com>
2018-04-16 14:55:45 +0800

    init commit refactor practice 2 and 3

--- src/main/java/practice1/SecurityChecker.java
+++ src/main/java/practice1/SecurityChecker.java
@@ -1,12 +1,15 @@
 package practice1;
 
 public class SecurityChecker {
-    public  boolean checkPermission(User user, Permission permission) {
-        //todo code
-        return false;
+    public boolean checkPermission(User user, Permission permission) {
+        if (user == null || permission == null){
+            return false;
+        }
+        return true;
     }
 
     public boolean isAdmin() {
+
         //todo code
         return false;
     }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,29 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+
+    public BigDecimal calculate(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax1) {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        // Subtract discounts
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+
+        // calculate tax
+        BigDecimal tax = subTotal.multiply(tax1);
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
 }
